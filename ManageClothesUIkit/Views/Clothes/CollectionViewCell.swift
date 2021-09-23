05fe92d1508c3c738fb28d693a371de086350f83
matsui kento <matsui_2000@matsuinoMacBook-Pro.local>
2021-09-23 16:17:15 +0900

    fix_ClothesViewController
    ClothesViewControllerのコードで作っていたUIをstoryboardでのUIに変更。

--- ManageClothesUIkit/Views/Clothes/CollectionViewCell.swift
+++ ManageClothesUIkit/Views/Clothes/CollectionViewCell.swift
@@ -6,27 +6,9 @@
 //
 
 import UIKit
-import SDWebImage
 
 class CollectionViewCell: UICollectionViewCell {
     
-    let imageView: UIImageView = {
-        let screenSize: CGSize = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
-        var imageview = UIImageView()
-        imageview.contentMode = .scaleToFill
-        imageview.frame = CGRect(x: 0, y: 0, width: screenSize.width / 3, height: screenSize.width / 3)
-        return imageview
-    }()
+    @IBOutlet weak var imageView: UIImageView!
     
-    var imageURLString = ""
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        contentView.addSubview(imageView)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 }
