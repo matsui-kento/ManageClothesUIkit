a6a63c41c0c2727dfefabeb23b368e668f0d8c6a
matsui kento <matsui_2000@matsuinoMacBook-Pro.local>
2021-09-27 21:17:41 +0900

    Create LoginViewModel
    LoginViewControllerのバリデーションに使うViewModelの作成。

---
+++ ManageClothesUIkit/ViewModels/LoginViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewModel.swift
+//  ManageClothesUIkit
+//
+//  Created by matsui kento on 2021/09/27.
+//
+
+import Foundation
+import RxSwift
+
+class LoginViewModel {
+    let emailTextPublishSubject = PublishSubject<String>()
+    let passwordTextPublishSubject = PublishSubject<String>()
+    
+    func isValid() -> Observable<Bool> {
+        return Observable.combineLatest(emailTextPublishSubject.asObservable(), passwordTextPublishSubject.asObservable())
+            .map { email, password in
+                return email.isValidEmail() && password.count >= 6
+            }
+    }
+}
